cc4c2af4598309745a8eb413b38e42cf1028c3ec
Matthew Struble <matt@vevox.io>
2016-07-25 14:55:03 -0700

    Refactor to vxStructures and create class

---
+++ src/main/java/io/vevox/vx/structures/vxStructures.java
@@ -0,0 +1,13 @@
+package io.vevox.vx.structures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main plugin class for vxStructures.
+ * @author Matthew Struble
+ */
+public class vxStructures extends JavaPlugin {
+
+
+
+}
